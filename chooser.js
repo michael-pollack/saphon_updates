19a5f49a61f559dd075d91f814cb323a916a4cf6
Michael Pollack <michaelppollack@gmail.com>
2024-03-27 15:15:18 -0700

    JSON generation update

--- chooser.js
+++ chooser.js
@@ -16,7 +16,7 @@ $(document).ready( function() {
       scrollTop: $('#chooser').offset().top
     }, 500);
   });
-
+ 
   function handle_click( span) {
     downloadUrl("../langs.json", function(langs) {
       if( span != null) {
@@ -133,14 +133,14 @@ $(document).ready( function() {
       var matches = 0;
       langset.clear()
       tableBody.innerHTML = '';
-      const rowMax = 6;
+      const rowMax = 20;
       var maxHit = false;
       var rowIndex = 0;
       var cellIndex = 0;
 
       langs.forEach(function (item) {
         if (item.faults == 0) {
-
+          matches += 1;
           if (!maxHit && rowIndex < rowMax) {
             var row = tableBody.insertRow();
             rowIndex += 1;
@@ -176,7 +176,6 @@ $(document).ready( function() {
         if( this.faults == 0) {
           $(this).css('display', 'block')
           langset.add(this.innerText)
-          matches += 1;
         } else {
           $(this).css('display', 'none')
         }
@@ -237,7 +236,8 @@ var icons = {
   'Yanomam' : getIcon( '#80c', 'Y'),
   'Zaparoan' : getIcon( '#fc4', 'Z'),
   'Chon' : getIcon( '#fef', 'Ch'),
-  'Other' : getIcon( '#ccd', '')
+  'Other' : getIcon( '#ccd', ''),
+  'TODO' : getIcon( '#ccd', '')
 };
  
 function get_pos(el) {
@@ -269,59 +269,72 @@ function initialize() {
   var tooltip = document.getElementById("tooltip");
 
   // get URL parameter c
-  var parm_c = decodeURIComponent((new RegExp('[?|&]c=' + '([^&;]+?)(&|#|;|$)')
-    .exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+  // var parm_c = decodeURIComponent((new RegExp('[?|&]c=' + '([^&;]+?)(&|#|;|$)')
+  //   .exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 
   downloadUrl("../langs.json", function(langs) {
     langs.forEach(function (lang){ 
       var title = lang.title; 
       if (langset.has(title)) {
-        var iso_code = lang.iso_code; 
         var family = lang.family;
         var link = lang.link;
-        var point = new google.maps.LatLng( 
-            parseFloat(lang.lat), 
-            parseFloat(lang.lng)); 
 
-        var marker = 
-            new StyledMarker({
-            map: map, 
-            position: point,
-                styleIcon: (family in icons ? icons[family] : icons.Other)
-            })
+        //needs to be iterative
+        //var iso_code = lang.iso_code; 
 
-            if( parm_c != null && parm_c == iso_code) {
-                map.panTo( point);
-                map.setZoom( 9);
-            }
-
-        google.maps.event.addListener(marker, 'mouseover', function() {
-                langinfo.innerHTML = "<span class=key>Language:</span> <b>" + title 
-                + "</b> <span class=key>Code:</span> <b>" + iso_code 
-                + "</b> <span class=key>Family:</span> <b>" + family + "</b>"; 
-            var projection = overlay.getProjection(); 
-            var pixel = projection.fromLatLngToContainerPixel( 
-                marker.getPosition());
-            tooltip.style.top = (map_pos.y + pixel.y - 60) + "px";
-            tooltip.style.left = (map_pos.x + pixel.x - 11) + "px";
-            tooltip.style.padding = "1px 2px"
-                tooltip.innerHTML = title;
-        });
-
-        google.maps.event.addListener(marker, 'mouseout', function() {
-                langinfo.innerHTML = ""
-                tooltip.innerHTML = ""
-            tooltip.style.padding = "0"
-        });
-
-        google.maps.event.addListener(marker, 'click', function( event) {
-                if( metadown) {
-                window.open(link);
-                } else {
-                window.location.href = link;
-                }
-        });   
+        var iso_codes = "";
+        for(var i = 0; i < lang.iso_code.length; i++) {
+          if (i > 0) {
+            iso_codes = iso_codes + ", "
+          }
+          iso_codes = iso_codes + lang.iso_code[i]
         }
+
+        //Needs to be iterative
+        for(var j = 0; j < lang.coordinates.length; j++) {
+          var coord = lang.coordinates[j];
+          var point = new google.maps.LatLng( 
+              parseFloat(coord.latitude), 
+              parseFloat(coord.longitude)); 
+          var marker = 
+              new StyledMarker({
+              map: map, 
+              position: point,
+                  styleIcon: (family in icons ? icons[family] : icons.Other)
+              })
+              //may not need this
+              // if( parm_c != null && parm_c == iso_code) {
+              //     map.panTo( point);
+              //     map.setZoom( 9);
+              // }
+          google.maps.event.addListener(marker, 'mouseover', function() {
+                  langinfo.innerHTML = "<span class=key>Language:</span> <b>" + title 
+                  + "</b> <span class=key>Code:</span> <b>" + iso_codes 
+                  + "</b> <span class=key>Family:</span> <b>" + family + "</b>"; 
+              var projection = overlay.getProjection(); 
+              var pixel = projection.fromLatLngToContainerPixel( 
+                  marker.getPosition());
+              tooltip.style.top = (map_pos.y + pixel.y - 60) + "px";
+              tooltip.style.left = (map_pos.x + pixel.x - 11) + "px";
+              tooltip.style.padding = "1px 2px"
+                  tooltip.innerHTML = title;
+          });
+
+          google.maps.event.addListener(marker, 'mouseout', function() {
+                  langinfo.innerHTML = ""
+                  tooltip.innerHTML = ""
+              tooltip.style.padding = "0"
+          });
+
+          google.maps.event.addListener(marker, 'click', function( event) {
+                  if( metadown) {
+                  window.open(link);
+                  } else {
+                  window.location.href = link;
+                  }
+          });   
+        }
+      }
   });
   })
 }
