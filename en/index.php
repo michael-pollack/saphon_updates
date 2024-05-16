dfaa208837e45ab906912c0faf4bc89df6052989
Michael Pollack <michaelppollack@gmail.com>
2024-05-16 10:08:03 -0700

    restructuring

--- en/index.php
+++ en/index.php
@@ -3,7 +3,7 @@
   <script type="text/javascript" src="https://maps.google.com/maps/api/js?v=quarterly&language=en&key=AIzaSyCBTjzFyFyz9h92Y9QQHzryzV565Edat5g"></script>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
   <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
-  <script type="text/javascript" src="map.js"></script>
+  <script type="text/javascript" src="/map.js"></script>
 </head>
 <body onload="initialize()">
 <?php include("title.php"); ?>
