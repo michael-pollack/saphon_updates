cb6d8c3724047b4d64c4d52382797edd8751bea7
Michael Pollack <michaelppollack@gmail.com>
2024-05-31 14:22:12 -0700

    added basic css sheet

--- html_generator.py
+++ html_generator.py
@@ -113,7 +113,7 @@ def generate_html(template):
     <html>
     <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link rel="stylesheet" type="text/css" href="../../inv.css" />
+    <link rel="stylesheet" type="text/css" href="../../lang_info.css" />
     </head>
     <body>
     <div class=entry>
